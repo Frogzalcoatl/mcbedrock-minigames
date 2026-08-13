172b4bdbceb6f5df6c2719db240757a34587b329
Frogzalcoatl <saharholmes@gmail.com>
2026-08-13 12:50:40 -0400

    Add all my decent looking structures

--- source/structures/data.ts
+++ source/structures/data.ts
@@ -1,9 +1,15 @@
 import type { Vector3 } from "@minecraft/server";
 import { PACK_NAMESPACE } from "../constants";
-import rawGhostlyMangrove from "./json/ghostlyMangrove.json" with { type: "json" };
-import rawGhostlyMangroveNoChests from "./json/ghostlyMangroveNoChests.json";
-import rawGhostlySpawn from "./json/ghostlySpawn.json" with { type: "json" };
-import rawKitPvpArena from "./json/kitPvpArena.json" with { type: "json" };
+import rawFrogzalcoatlBridgeEnd from "./json/frogzalcoatl/bridge/end.json";
+import rawFrogzalcoatlDuelsMangrove from "./json/frogzalcoatl/duels/mangrove.json";
+import rawFrogzalcoatlLobbyFrogland from "./json/frogzalcoatl/lobby/frogland.json";
+import rawFrogzalcoatlLobbyHaroldsRealm from "./json/frogzalcoatl/lobby/haroldsRealm.json";
+import rawFrogzalcoatlSkywarsExotic from "./json/frogzalcoatl/skywars/exotic.json";
+import rawFrogzalcoatlSurvivalGamesOverworld from "./json/frogzalcoatl/survivalGames/overworld.json";
+import rawGhostlyKitPvp from "./json/ghostly/kitPvp.json" with { type: "json" };
+import rawGhostlyMangrove from "./json/ghostly/mangrove.json" with { type: "json" };
+import rawGhostlyMangroveNoChests from "./json/ghostly/mangroveNoChests.json";
+import rawGhostlySpawn from "./json/ghostly/spawn.json" with { type: "json" };
 
 type JsonStructureEntry = [string, number, number, number]; // [structureId, relativeX, relativeY, relativeZ]
 type StructureSchema = JsonStructureEntry[];
@@ -11,7 +17,15 @@ type StructureSchema = JsonStructureEntry[];
 const ghostlySpawn = rawGhostlySpawn as unknown as StructureSchema;
 const ghostlyMangrove = rawGhostlyMangrove as unknown as StructureSchema;
 const ghostlyMangroveNoChests = rawGhostlyMangroveNoChests as unknown as StructureSchema;
-const kitPvpArena = rawKitPvpArena as unknown as StructureSchema;
+const ghostlyKitPvp = rawGhostlyKitPvp as unknown as StructureSchema;
+const frogzalcoatlDuelsMangrove = rawFrogzalcoatlDuelsMangrove as unknown as StructureSchema;
+const frogzalcoatlSkywarsExotic = rawFrogzalcoatlSkywarsExotic as unknown as StructureSchema;
+const frogzalcoatlBridgeEnd = rawFrogzalcoatlBridgeEnd as unknown as StructureSchema;
+const frogzalcoatlLobbyFrogland = rawFrogzalcoatlLobbyFrogland as unknown as StructureSchema;
+const frogzalcoatlSurvivalGamesOverworld =
+	rawFrogzalcoatlSurvivalGamesOverworld as unknown as StructureSchema;
+const frogzalcoatlLobbyHaroldsRealm =
+	rawFrogzalcoatlLobbyHaroldsRealm as unknown as StructureSchema;
 
 export type StructureInfo = {
 	id: string;
@@ -22,10 +36,38 @@ const structureSchemas = new Map<string, StructureSchema>([
 	["ghostly/spawn", ghostlySpawn],
 	["ghostly/mangrove", ghostlyMangrove],
 	["ghostly/mangroveNoChests", ghostlyMangroveNoChests],
-	["ghostly/kitPvp", kitPvpArena],
+	["ghostly/kitPvp", ghostlyKitPvp],
+	["frogzalcoatl/duels/mangrove", frogzalcoatlDuelsMangrove],
+	["frogzalcoatl/skywars/exotic", frogzalcoatlSkywarsExotic],
+	["frogzalcoatl/bridge/end", frogzalcoatlBridgeEnd],
+	["frogzalcoatl/lobby/frogland", frogzalcoatlLobbyFrogland],
+	["frogzalcoatl/survivalGames/overworld", frogzalcoatlSurvivalGamesOverworld],
+	["frogzalcoatl/lobby/haroldsRealm", frogzalcoatlLobbyHaroldsRealm],
 ]);
 
-export const structureIds: string[] = ["ghostly/crates", "ghostly/tree", "ghostly/mountain"];
+export const structureIds: string[] = [
+	"ghostly/crates",
+	"ghostly/tree",
+	"ghostly/mountain",
+	"frogzalcoatl/duels/mooshroom",
+	"frogzalcoatl/duels/sumo",
+	"frogzalcoatl/skywars/waitingArea",
+	"frogzalcoatl/lobby/minersRealm",
+	"frogzalcoatl/bedwars/chambers/base",
+	"frogzalcoatl/bedwars/chambers/diamonds",
+	"frogzalcoatl/bedwars/chambers/mid",
+	"frogzalcoatl/bedwars/desert/base",
+	"frogzalcoatl/bedwars/desert/diamonds",
+	"frogzalcoatl/bedwars/desert/mid",
+	"frogzalcoatl/bedwars/desert/extra",
+	"frogzalcoatl/bedwars/netherlands/base",
+	"frogzalcoatl/bedwars/netherlands/diamonds1",
+	"frogzalcoatl/bedwars/netherlands/diamonds2",
+	"frogzalcoatl/bedwars/netherlands/mid",
+	"frogzalcoatl/bedwars/template/base",
+	"frogzalcoatl/bedwars/template/diamonds",
+	"frogzalcoatl/bedwars/template/mid",
+];
 structureIds.push(...structureSchemas.keys());
 
 function schemaToStructureInfo(schema: StructureSchema): StructureInfo[] {
