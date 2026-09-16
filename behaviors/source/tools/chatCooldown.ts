ba1f30027669029e17d5e4f2060b640e04467c92
Frogzalcoatl <saharholmes@gmail.com>
2026-09-16 13:05:57 -0400

    Copy hive chat formatting for now

--- behaviors/source/tools/chatCooldown.ts
+++ behaviors/source/tools/chatCooldown.ts
@@ -4,12 +4,15 @@ const timestamps = new Map<string, number>();
 const cooldownMs: number = 500;
 
 world.beforeEvents.chatSend.subscribe((event: ChatSendBeforeEvent) => {
+	event.cancel = true;
 	const timestamp: number | undefined = timestamps.get(event.sender.id);
 	if (timestamp !== undefined && timestamp + cooldownMs > Date.now()) {
-		event.cancel = true;
 		event.sender.sendMessage("§cYou are sending messages too fast!");
 		return;
 	}
+	world.sendMessage(
+		`${event.sender.chatNamePrefix ?? "§7"}${event.sender.name}${event.sender.chatNameSuffix ?? ""} §r§l§7»§r ${event.message}`,
+	);
 	timestamps.set(event.sender.id, Date.now());
 });
 
