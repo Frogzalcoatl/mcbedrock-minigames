62a6bdf999e682f0401b085d33dd154554d895f3
Frogzalcoatl <saharholmes@gmail.com>
2026-09-17 16:30:44 -0400

    Default death message color is gray

--- behaviors/source/tools/deathMessages.ts
+++ behaviors/source/tools/deathMessages.ts
@@ -30,7 +30,7 @@ export function deathMessageFormat(
 	}
 }
 
-export function deathMessageFromEvent(event: EntityDieAfterEvent, colorCode = "§f"): string {
+export function deathMessageFromEvent(event: EntityDieAfterEvent, colorCode = "§7"): string {
 	const deadName: string = getEntityName(event.deadEntity);
 	let killerName: string | null = null;
 	if (event.damageSource.damagingEntity?.isValid) {
