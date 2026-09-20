d8d2b4dbe56ed0e70281c7ebac82c297e889899d
Frogzalcoatl <saharholmes@gmail.com>
2026-09-20 04:35:09 -0400

    Add leave game item

---
+++ behaviors/source/items/games/leaveGame.ts
@@ -0,0 +1,43 @@
+import {
+	EntitySwingSource,
+	HeldItemOption,
+	ItemLockMode,
+	type ItemStack,
+	type ItemUseAfterEvent,
+	type PlayerSwingStartAfterEvent,
+	world,
+} from "@minecraft/server";
+import { MinecraftItemTypes } from "@minecraft/vanilla-data";
+import { roomTypeIds } from "../../constants";
+import { defaultItemStackFunc } from "../../tools/componentHelpers";
+import { roomTypeIdJoin } from "../../tools/rooms/roomType";
+import { itemUseMap } from "../events";
+
+const typeId: string = MinecraftItemTypes.RedDye;
+const nameTag: string = "§r§cLeave";
+
+itemUseMap.set(nameTag, {
+	callback: (event: ItemUseAfterEvent): void => {
+		roomTypeIdJoin(event.source, roomTypeIds.hub);
+	},
+	typeId: typeId,
+});
+
+world.afterEvents.playerSwingStart.subscribe(
+	(event: PlayerSwingStartAfterEvent) => {
+		if (
+			event.heldItemStack !== undefined &&
+			event.heldItemStack.typeId === typeId &&
+			event.heldItemStack.nameTag === nameTag
+		) {
+			roomTypeIdJoin(event.player, roomTypeIds.hub);
+		}
+	},
+	{ heldItemOption: HeldItemOption.AnyItem, swingSource: EntitySwingSource.Attack },
+);
+
+export function itemLeaveGame(): ItemStack {
+	const item: ItemStack = defaultItemStackFunc(typeId, nameTag);
+	item.lockMode = ItemLockMode.inventory;
+	return item;
+}
